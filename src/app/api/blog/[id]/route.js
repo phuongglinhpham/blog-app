76316ef949b521623679d0acabeb0544fa567264
phuongglinhpham <79841701+phuongglinhpham@users.noreply.github.com>
2023-06-15 13:38:37 +0700

    Update route.js

--- src/app/api/blog/[id]/route.js
+++ src/app/api/blog/[id]/route.js
@@ -1,23 +1,28 @@
 import db from "@/lib/db";
-import { verifyJwtToken, verifyToken } from '@/lib/jwt'
+import { verifyJwtToken } from "@/lib/jwt";
 import Blog from "@/models/Blog";
+import User from "@/models/User";
 
-export async function GET(req) {
+export async function GET(req, ctx) {
     await db.connect()
 
+    const id = ctx.params.id
+
     try {
-        const blogs = await Blog.find({}).limit(16).populate("authorId")
-        return new Response(JSON.stringify(blogs), { status: 200 })
+        const blog = await Blog.findById(id).populate("authorId").select('-password')
+
+        return new Response(JSON.stringify(blog), { status: 200 })
     } catch (error) {
         return new Response(JSON.stringify(null), { status: 500 })
     }
 }
 
-export async function POST(req) {
+export async function PUT(req, ctx) {
     await db.connect()
 
-    const accessToken = req.headers.get("authorization")
-    const token = accessToken.split(' ')[1]
+    const id = ctx.params.id
+    const accessToken = req.headers.get('authorization')
+    const token = accessToken.split(" ")[1]
 
     const decodedToken = verifyJwtToken(token)
 
@@ -27,10 +32,49 @@ export async function POST(req) {
 
     try {
         const body = await req.json()
-        const newBlog = await Blog.create(body)
+        const blog = await Blog.findById(id).populate('authorId')
 
-        return new Response(JSON.stringify(newBlog), { status: 201 })
+        if (blog?.authorId?._id.toString() !== decodedToken._id.toString()) {
+            return new Response(JSON.stringify({ msg: 'Only author can update his blog' }), { status: 403 })
+        }
+
+        const updatedBlog = await Blog.findByIdAndUpdate(id, { $set: { ...body } }, { new: true })
+
+        return new Response(JSON.stringify(updatedBlog), { status: 200 })
     } catch (error) {
         return new Response(JSON.stringify(null), { status: 500 })
     }
 }
+
+export async function DELETE(req, ctx) {
+    await db.connect()
+
+    const id = ctx.params.id
+
+    const accessToken = req.headers.get('authorization')
+    const token = accessToken.split(' ')[1]
+
+    const decodedToken = verifyJwtToken(token)
+
+    if (!accessToken || !decodedToken) {
+        return new Response(JSON.stringify({ error: "unauthorized (wrong or expired token)" }), { status: 403 })
+    }
+
+    try {
+        const blog = await Blog.findById(id).populate('authorId')
+        if (blog?.authorId?._id.toString() !== decodedToken._id.toString()) {
+            return new Response(JSON.stringify({ msg: 'Only author can delete his blog' }), { status: 403 })
+        }
+
+        await Blog.findByIdAndDelete(id)
+
+        return new Response(JSON.stringify({msg: 'Successfully deleted blog'}), {status: 200})
+    } catch (error) {
+        return new Response(JSON.stringify(null), { status: 500 }) 
+    }
+}
+
+// blog -> [id] -> like -> route.js
+
+
+// http://localhost:3000/api/blog/someid/like
